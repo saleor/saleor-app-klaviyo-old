193f6acc8af781d274f13e2ae8875b75d820b692
timur <timuric@gmail.com>
2022-03-11 10:00:08 +0200

    Replace db with env variable

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,13 +1,11 @@
 import {
   CreateWebhookMutation,
   WebhookEventTypeEnum,
-} from "./../../graphql/generated/graphql";
+} from "../../graphql/generated/graphql";
 import { NextApiRequest, NextApiResponse } from "next";
 import { SALEOR_DOMAIN_HEADER } from "../../constants";
 import { createWebhook } from "../../graphql/data/mutations/webhook";
-import { initializeApollo } from "../../graphql/withApollo";
-import { Registration } from "../../saleorApp/entities/Registration.entity";
-import { getOrCreateConnection } from "../../utils/database";
+import { saleor } from "../../graphql/client";
 
 const handler = async (
   request: NextApiRequest,
@@ -31,20 +29,11 @@ const handler = async (
     res.end(JSON.stringify({ success: false, message: "Missing auth token." }));
     return;
   }
-  const conn = await getOrCreateConnection();
-  const registrationRepo = conn.getRepository<Registration>(Registration);
-  const newRegistration = new Registration();
-  const domain = saleor_domain.toString();
-  newRegistration.domain = domain;
-  newRegistration.authToken = auth_token;
-  await registrationRepo.insert(newRegistration);
 
   // async ftw
   res.end(JSON.stringify({ success: true }));
 
-  // webhook registration
-  const client = initializeApollo();
-  await client.mutate<CreateWebhookMutation>({
+  await saleor.mutate<CreateWebhookMutation>({
     mutation: createWebhook,
     variables: {
       name: "Best app: Product updated",
@@ -54,7 +43,7 @@ const handler = async (
     },
     context: {
       headers: {
-        Authorization: `Bearer ${newRegistration.authToken}`,
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
       },
     },
   });
