265d341f26da3c6754926fa762e83b0d0154be34
timur <timuric@gmail.com>
2022-03-11 10:00:52 +0200

    Add sample query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import Layout from "../components/Layout";
+import { gql, useQuery } from "@apollo/client";
 
-const IndexPage = () => (
-  <Layout title="Home">
-    <h1>👋</h1>
-  </Layout>
-);
+const IndexPage = () => {
+  const { data } = useQuery(gql`
+    {
+      shop {
+        domain {
+          host
+          url
+        }
+        name
+      }
+    }
+  `);
+
+  return (
+    <Layout title="Home">
+      <h1>👋</h1>
+      <div>{data}</div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
