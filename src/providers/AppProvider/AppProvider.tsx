4b4c82b5667762961eb69f34d8438ece2c795a24
Wojciech <wojciech.mista@hotmail.com>
2022-04-20 11:58:12 +0200

    Create app-bridge app provider

---
+++ src/providers/AppProvider/AppProvider.tsx
@@ -0,0 +1,19 @@
+import { App, createApp } from "@saleor/app-bridge";
+import { createContext, useMemo } from "react";
+
+interface IAppContext {
+  app?: App;
+}
+
+export const AppContext = createContext<IAppContext>({ app: undefined });
+
+const AppProvider: React.FC = (props) => {
+  const app = useMemo(() => {
+    if (typeof window !== "undefined") {
+      return createApp();
+    }
+  }, []);
+
+  return <AppContext.Provider value={{ app }} {...props} />;
+};
+export default AppProvider;
