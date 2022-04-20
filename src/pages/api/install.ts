d05d57ce384c5417ec34b6bc6514b1eb5d11a923
Wojciech <wojciech.mista@hotmail.com>
2022-04-20 12:26:48 +0200

    Temporary hot fix for api install

--- src/pages/api/install.ts
+++ src/pages/api/install.ts
@@ -1,11 +1,13 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {saleor} from "../../graphql/client";
-import {gql} from "@apollo/client";
+import { NextApiRequest, NextApiResponse } from "next";
+// import { createApolloClient } from "../../graphql/client";
+// import { gql } from "@apollo/client";
 
-export default (req: NextApiRequest, res: NextApiResponse)=>{
-    saleor.mutate({
-        mutation: gql`
-        
-        `
-    })
-}
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  // TO-DO
+  // This will be changed with future PR to use different middleware
+  // It's out of scope for this PR
+  //   const client = createApolloClient();
+  //   client.mutate({
+  //     mutation: gql``,
+  //   });
+};
