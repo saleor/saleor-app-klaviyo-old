52dc2737d407c6b035d7be28405bbc6d70c1817a
Wojciech <wojciech.mista@hotmail.com>
2022-04-20 11:56:15 +0200

    Add apollo wrapper and theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,34 @@
 import type { AppProps } from "next/app";
+import * as React from "react";
 import Head from "next/head";
 import { ApolloProvider } from "@apollo/client";
-import { saleor } from "../graphql/client";
+import { createApolloClient } from "../graphql/client";
+import AppProvider from "../providers/AppProvider";
+import { useApp } from "../hooks/useApp";
+import { ThemeProvider } from "@saleor/macaw-ui";
+
+const ApolloWrapper: React.FC = ({ children }) => {
+  const app = useApp();
+
+  if (!app) {
+    return null;
+  }
+
+  const { token, domain } = app.getState()!;
+
+  const saleor = createApolloClient(token, domain);
+
+  return <ApolloProvider client={saleor}>{children}</ApolloProvider>;
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -11,9 +36,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <ApolloProvider client={saleor}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <AppProvider>
+        <ApolloWrapper>
+          <ThemeProvider>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ApolloWrapper>
+      </AppProvider>
     </>
   );
 }
