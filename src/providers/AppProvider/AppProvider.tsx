643a327ec6b494c403f063efe80bb37531b300e0
Wojciech <wojciech.mista@hotmail.com>
2022-04-20 13:07:09 +0200

    Wrap app-bridge creation in try...catch

--- src/providers/AppProvider/AppProvider.tsx
+++ src/providers/AppProvider/AppProvider.tsx
@@ -10,7 +10,11 @@ export const AppContext = createContext<IAppContext>({ app: undefined });
 const AppProvider: React.FC = (props) => {
   const app = useMemo(() => {
     if (typeof window !== "undefined") {
-      return createApp();
+      try {
+        return createApp();
+      } catch (e) {
+        console.error(e);
+      }
     }
   }, []);
 
