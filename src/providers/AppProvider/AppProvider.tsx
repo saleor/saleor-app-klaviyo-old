1641142fcc97bf4efcbcafab7adc1cef0d7098c6
Wojciech <wojciech.mista@hotmail.com>
2022-04-24 17:00:13 +0200

    Install new app-bridge

--- src/providers/AppProvider/AppProvider.tsx
+++ src/providers/AppProvider/AppProvider.tsx
@@ -1,4 +1,4 @@
-import { App, createApp } from "@saleor/app-bridge";
+import { createApp, App } from "@saleor/app-bridge";
 import { createContext, useMemo } from "react";
 
 interface IAppContext {
@@ -10,7 +10,11 @@ export const AppContext = createContext<IAppContext>({ app: undefined });
 const AppProvider: React.FC = (props) => {
   const app = useMemo(() => {
     if (typeof window !== "undefined") {
-      return createApp();
+      try {
+        return createApp();
+      } catch (e) {
+        console.error(e);
+      }
     }
   }, []);
 
